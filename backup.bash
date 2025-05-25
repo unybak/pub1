96a1b6621b30268590d4bbf548e3be8eb89f6940
Micha Cassola <mich@casso.la>
2025-05-25 16:58:22 +0200

    Update backup.bash

--- backup.bash
+++ backup.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034,SC1091,SC2154
 
-echo "Testing logging"
+echo "Testing Backup Script run"
 
 {
-    echo "Will this be shown?"
-    echo "Or not?"
-} >file
+    gh repo clone unybak/unybak
+
+} >backup.log
