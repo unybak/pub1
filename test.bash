8eda09c39a79ec3a4da04b33864e3343e64ec30f
Micha Cassola <mich@casso.la>
2025-06-10 21:23:19 +0200

    Testing

---
+++ test.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034,SC1091,SC2154
+
+echo "Testing"
+
+{
+    GH_TOKEN="$(cat GH_TOKEN)"
+    export GH_TOKEN
+
+    gh repo clone unybak/unybak
+    chmod +x unybak/test.bash
+    unybak/test.bash
+
+} >test.log 2>&1
