84028b7d0e3f2b57543e56a77272e0e6d7709df5
Micha Cassola <mich@casso.la>
2025-06-12 18:16:28 +0200

    Test Orchestration

--- test-orchestration.bash
+++ test-orchestration.bash
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034,SC1091,SC2154
 
-echo "Testing"
-
-servers="$*"
+echo "Test Orchestration"
 
 GH_TOKEN="$(cat GH_TOKEN)"
 export GH_TOKEN
@@ -14,5 +12,5 @@ export GITHUB_REPOSITORY
 gh repo clone unybak/unybak
 cd unybak || exit
 
-chmod +x test_prep_and_run.bash
-./test_prep_and_run.bash
+chmod +x test-orchestration.bash
+./test-orchestration.bash
