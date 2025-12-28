2afc240508396a97e136fe1961a1cd55f6ae8716
Micha Cassola <mich@casso.la>
2025-12-28 19:06:28 +0100

    Orch changes

--- orchestration.bash
+++ orchestration.bash
@@ -3,6 +3,11 @@
 
 echo "Orchestrating"
 
+# ssh_config_file is by default ssh_config
+config_repo="$1"
+# exec_repo is by default unybak/pub1
+exec_repo="$2"
+
 GH_TOKEN="$(cat GH_TOKEN)"
 export GH_TOKEN
 
@@ -12,5 +17,7 @@ export GITHUB_REPOSITORY
 gh repo clone unybak/unybak
 cd unybak || exit
 
+gh repo clone "$config_repo" config
+
 chmod +x orchestration.bash
-./orchestration.bash
+./orchestration.bash "$exec_repo"
